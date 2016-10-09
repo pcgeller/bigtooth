ae0b7d329fe4a663bca1f7cb8c5c1ea2f4953bba
pcgeller <pcgeller@gmail.com>
2016-10-08 20:46:23 -0400

    fixed pi boot up, started python script to mv bh output

--- rotate.sh
+++ rotate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 today=`date '+%Y_%m_%d__%H'`;
-dbfilename="/home/pcgeller/bigtooth/db/$today.pgsql.csv"
+dbfilename="/home/pcgeller/bigtooth/db/$today.sqlite.db"
 echo $dbfilename;
+cp /home/pcgeller/bigtooth/blue_hydra/blue_hydra.db $dbfilename
 
-logfilename="home/pcgeller/bigtooth/logs/$today.bhydra.log"
+logfilename="/home/pcgeller/bigtooth/logs/$today.bhydra.log"
 echo $logfilename;
+cp /home/pcgeller/bigtooth/blue_hydra/blue_hydra.log $logfilename
