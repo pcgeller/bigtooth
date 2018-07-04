90897bc3e90b8f176dfa5f7b98104807a0d551c3
pcgeller <pcgeller@gmail.com>
2018-07-03 22:29:32 -0400

    play w/ lights

--- dev/statusLights/scratchLight.py
+++ dev/statusLights/scratchLight.py
@@ -1,9 +1,21 @@
 import sys
-sys.path.insert(0, '/home/pcgeller/bigtooth/')
+sys.path.insert(0, '/home/pcgeller/bigtooth/dev')
+import statusLight
 
-lights = [('bigtooth', 21), ('blueHydra', 26), ('extra', 20)]
-lights["blueHydra":21]
+lightConfig = {'bigtooth': 21, 'blueHydra': 26, 'extra': 20}
+#lights["blueHydra":21]
 
+def initLights(config):
+    for name, gpio in config.items():
+        lights = []
+        light = statusLight(name, gpio)
+        lights.append(light)
+        
+
+
+
+
+    light = statusLight("blueHydraLED", 21)
 light = statusLight("blueHydra", 21)
 light.turnOn()
 light.turnOff()
