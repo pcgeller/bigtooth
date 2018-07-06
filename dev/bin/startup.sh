bb57ec7a94320b55ae6c9b4a3cfe12190cf92e42
pcgeller <pcgeller@gmail.com>
2018-07-06 14:00:41 +0000

    fix startup

--- dev/bin/startup.sh
+++ dev/bin/startup.sh
@@ -4,8 +4,8 @@
 sudo ifup wlan0
 sudo service hostapd status
 sh $1/bigtooth/dev/bin/rotate.sh $1
-pushd $1/data/bh/current
+cd $1/data/bh/current
 sudo $1/blueHydra/bin/blue_hydra -d &
-popd
+cd /
 python3 $1/bigtooth/dev/gpsSQlite.py > /home/pi/scratch/python &
 exit 0
