ff3fa4ddbc523872fc0f88971d23ca1b4b2f3a76
pcgeller <pcgeller@gmail.com>
2018-07-06 14:20:53 +0000

    print more

--- dev/bin/startup.sh
+++ dev/bin/startup.sh
@@ -6,6 +6,8 @@ sudo service hostapd status
 sh $1/bigtooth/dev/bin/rotate.sh $1
 cd $1/data/bh/current
 sudo $1/blueHydra/bin/blue_hydra -d &
+echo "Blue Hydra started."
 cd /
 python3 $1/bigtooth/dev/gpsSQlite.py > /home/pi/scratch/python &
+echo "GPS logger started."
 exit 0
